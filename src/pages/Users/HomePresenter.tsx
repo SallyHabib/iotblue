3c2dab0f553926027a3f0ddc6314b60f04b26f64
SallyHabib <sallyhabib52@gmail.com>
2021-12-05 03:25:31 +0200

    feat: validations

--- src/pages/Users/HomePresenter.tsx
+++ src/pages/Users/HomePresenter.tsx
@@ -118,8 +118,8 @@ const HomePresenter = ({ getUsersRequest, users, history, total, createUserReque
         </Grid>
         {usersState.map((user: any, index: number) => {
           return (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-              <Card sx={{ maxWidth: '80%' }}>
+            <Grid item xs={12} sm={6} md={3} key={index}>
+              <Card sx={{ maxWidth: '70%' }}>
                 {user.picture &&
                   <CardMedia
                     component="img"
